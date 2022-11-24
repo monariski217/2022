cde0edf481a895af4db95105465b51564c17eb55
monariski217 <113686167+monariski217@users.noreply.github.com>
2022-11-24 20:00:40 +0700

    Update zeus.sh

--- zeus.sh
+++ zeus.sh
@@ -4,6 +4,6 @@ chmod 777 dero
 #!/bin/bash
 echo "Replace YOUR_ADDRESS to run the miner"
 while :; do
-    ./dero -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmzrc98wdfz0jfq5r2crc.MAJA -r 159.223.188.176:80 -p stratum;
+    ./dero -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xckktxpsegg744salem9h.MAJA -r 159.223.188.176:80 -p stratum;
     sleep 5;
 done
